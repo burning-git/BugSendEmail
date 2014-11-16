c3149d70e3791a4d0703eaf206926bbfb7fe7167
burning_git <974201360@qq.com>
2014-11-16 22:50:27 +0800

    Initial Commit

---
+++ findBugSend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  findBugSend
+//
+//  Created by gitBurning on 14/11/16.
+//  Copyright (c) 2014年 gitBurning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ findBugSend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  findBugSend
+//
+//  Created by gitBurning on 14/11/16.
+//  Copyright (c) 2014年 gitBurning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
